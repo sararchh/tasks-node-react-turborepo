519abfd63925ea6705ada8b9007f7ad4f98f82ee
sararchh <sara_rocha99@hotmail.com>
2025-09-04 18:01:29 -0300

    feat: add TypeOrmModule

--- apps/notifications-service/src/app.module.ts
+++ apps/notifications-service/src/app.module.ts
@@ -1,9 +1,23 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST || 'localhost',
+      port: process.env.DB_PORT ? parseInt(process.env.DB_PORT) : 5432,
+      username: process.env.DB_USERNAME || 'postgres',
+      password: process.env.DB_PASSWORD || 'password',
+      database: process.env.DB_NAME || 'challenge_db',
+      entities: [],
+      synchronize: process.env.NODE_ENV !== 'production',
+      logging: process.env.NODE_ENV === 'development',
+    }),
+    TypeOrmModule.forFeature([]),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
