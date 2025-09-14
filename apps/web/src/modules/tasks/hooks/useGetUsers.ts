22a1648975b74c5696f4bfac97734e93dda12627
sararchh <sara_rocha99@hotmail.com>
2025-09-14 08:05:45 -0300

    fix: adjust token

--- apps/web/src/modules/tasks/hooks/useGetUsers.ts
+++ apps/web/src/modules/tasks/hooks/useGetUsers.ts
@@ -2,12 +2,8 @@ import { useQuery } from '@tanstack/react-query';
 import { getUsersService } from '../services/get-users';
 
 export function useGetUsers() {
-  const hasToken = !!localStorage.getItem('accessToken');
-  
   return useQuery({
     queryKey: ['users'],
     queryFn: () => getUsersService.execute(),
-    staleTime: 1000 * 60 * 10, // 10 minutes
-    enabled: hasToken, // Só executa se há token
   });
 }
